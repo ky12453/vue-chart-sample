d28bc5bb588df91f3c7beea62690b1104a2dfa58
ky12453 <ky12453@gmail.com>
2020-07-12 23:04:07 +0900

    PB_CV_001_breakpoint

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-card color="#FF6633" height="200">
+            <v-card-title>card title</v-card-title>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-card color="#FF6633" height="200">
+            <v-card-title>card title</v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
