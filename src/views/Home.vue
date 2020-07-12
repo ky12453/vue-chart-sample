757d15373ed2e8d4e4971df19a337127443c2213
ky12453 <ky12453@gmail.com>
2020-07-13 07:21:38 +0900

    PB_CV_002_card内コンテンツ整列

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,11 @@
       <v-row>
         <v-col cols="12" md="6">
           <v-card color="#FF6633" height="200">
-            <v-card-title>card title</v-card-title>
+            <div class="card">
+              <h3>card title</h3>
+              <p>card content card content card content card content card content card content card content card content </p>
+              <v-btn class="btn">CLICK</v-btn>
+            </div>
           </v-card>
         </v-col>
         <v-col cols="12" md="6">
@@ -26,4 +30,16 @@ export default class Home extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.card {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  position: relative;
+
+  .btn {
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+  }
+}
 </style>
