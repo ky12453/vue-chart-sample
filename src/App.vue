19cc633a316b258dac657b6cd58cd9421fb2a190
ky12453 <ky12453@gmail.com>
2020-07-12 23:01:52 +0900

    初回コミット

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <router-view/>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+  data: () => ({
+    //
+  }),
+});
+</script>
